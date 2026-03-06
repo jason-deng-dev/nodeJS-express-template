152e1486a7679242a47e1d809030c69b3933af80
Jason <jason13.deng@gmail.com>
2026-03-06 11:14:48 +0900

    Setup ejs in app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+// app.js
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const path = require("node:path");
+
 app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = 3000;
