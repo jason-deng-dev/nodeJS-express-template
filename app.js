d2096e5efb38204c65b693b9797e51e1e6c28355
Jason <jason13.deng@gmail.com>
2026-03-08 18:43:38 +0900

    fix: clean app.js, add body parser for form POST

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
 // setup for static assets
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
-
-
 // setup for EJS
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-const path = require("node:path");
 
-// Setup for express validator
 const { body, validationResult } = require("express-validator");
+// body parser for form POST
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -21,9 +20,6 @@ app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
-  // This is important!
-  // Without this, any startup errors will silently fail
-  // instead of giving you a helpful error message.
   if (error) {
     throw error;
   }
