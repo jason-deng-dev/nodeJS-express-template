7d02c209a2150e47599ab09bb262f3f8494b047b
Jason <jason13.deng@gmail.com>
2026-03-09 14:15:48 +0900

    modern css reset

--- db/pools.js
+++ db/pools.js
@@ -1,11 +1,9 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
+  host: "localhost", 
   user: "<role_name>",
   database: "top_users",
   password: "<role_password>",
-  port: 5432 // The default port
+  port: 5432 
 });
