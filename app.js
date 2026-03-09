fd7dda71c7b40b58559b5eb7bf72bf832fb15661
Jason <jason13.deng@gmail.com>
2026-03-09 14:18:46 +0900

    Setup default files

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("node:path");
+const path = require('node:path');
+const indexRouter = require('./routes/indexRouter');
 
 // setup for static assets
-const assetsPath = path.join(__dirname, "public");
+const assetsPath = path.join(__dirname, 'public');
 app.use(express.static(assetsPath));
 // setup for EJS
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-const { body, validationResult } = require("express-validator");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // body parser for form POST
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/', indexRouter);
 
-
-
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use((err, req, res, next) => {
+	console.error(err);
+	res.status(500).send('ERROR');
+});
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
-  if (error) {
-    throw error;
-  }
-  console.log(`My first Express app - listening on port ${PORT}!`);
+	if (error) {
+		throw error;
+	}
+	console.log(
+		`My first Express app - listening on port ${PORT}!`,
+	);
 });
